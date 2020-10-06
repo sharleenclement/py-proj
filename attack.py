ac22b103ddb8bb55d0ab4bbef790f1a1fb8a7c8f
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2020-10-06 17:32:48 +0530

    RPG

---
+++ attack.py
@@ -0,0 +1,18 @@
+import random
+
+playerhp = 300
+enemykl = 50
+enemykh = 100
+
+while playerhp > 0:
+    damage = random.randrange(enemykl, enemykh) # generates random values between start and end point.
+    playerhp-=damage
+    
+    if playerhp <= 50:
+        playerhp = 50
+    
+    print("The damage caused by the enemy is", damage, "and the health of the player is", playerhp)
+    
+    if playerhp == 50:
+        print("You have low health. You've been teleported to the nearest inn.")
+        break
