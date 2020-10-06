f374e15a0b6598db86075cb57de9c3910ba6badb
Sharleen clement <51881069+sharleenclement@users.noreply.github.com>
2020-10-06 18:19:37 +0530

    Update attack.py

--- attack.py
+++ attack.py
@@ -2,17 +2,24 @@ import random
 
 class Enemy:
     
+    hp = 200
+    
     def __init__(self, atkl, atkh):
         self.atkl = atkl
         self.atkh = atkh
     
     def getAtk(self):
-        print(self.atkl)
+        print("Attack is:", self.atkl)
+    
+    def getHp(self):
+        print("Hp is:", self.hp)
 
 enemy1 = Enemy(90, 100)
 enemy1.getAtk()
+enemy1.getHp()
 enemy2 = Enemy(30, 70)
 enemy2.getAtk()
+enemy2.getHp()
     
     
 playerhp = 300
